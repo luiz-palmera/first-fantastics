6443313c06ab9ce96ca94ca556ead6872fee2f6b
Luiz da Mata <luizrdamata@gmail.com>
2026-04-17 14:40:55 -0300

    refactor: improve theme types

--- src/theme/styled.d.ts
+++ src/theme/styled.d.ts
@@ -5,5 +5,14 @@ import { theme } from './theme';
 type Theme = typeof theme;
 
 declare module 'styled-components' {
-    export interface DefaultTheme extends Theme {}
+    export interface DefaultTheme {
+        colors: Theme['colors'];
+        fonts: Theme['fonts'];
+        fontSizes: Theme['fontSizes'];
+        spacing: Theme['spacing'];
+        borderRadius: Theme['borderRadius'];
+        shadows: Theme['shadows'];
+        breakpoints: Theme['breakpoints'];
+        zIndex: Theme['zIndex'];
+    }
 }
