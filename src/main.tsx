b5c791b26166815a21911b262da14b5ceda01f73
Luiz da Mata <luizrdamata@gmail.com>
2026-04-17 14:37:20 -0300

    fix: remove useless imports from main

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { ThemeProvider } from 'styled-components'
-import { theme } from './theme/theme.ts'
 import { GlobalStyle } from './styles/GlobalStyle.ts'
 import { CustomThemeProvider } from './context/ThemeContext.tsx'
 
